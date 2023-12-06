f85163f7e699d1cd0a50c59fbb2abd5b92479d55
Jean de Gruben <jeandegruben@gmail.com>
2023-12-07 00:07:07 +0100

    more readable code and structure

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,3 +1,4 @@
+Copy code
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -10,130 +11,80 @@ import matplotlib.dates as mdates
 import plotly.express as px
 import plotly.graph_objects as go
 
-
-
-
-#####################################################
-#STYLING
-#####################################################
+# ------------------------------------------------------------------------------------------
+#                           Set page configuration
+# ------------------------------------------------------------------------------------------
 
 st.set_page_config(page_title='Electricity Prediction', layout='wide', page_icon=':zap:')
-
 st.markdown("""
     <style>
     body {
         font-family: 'Helvetica', sans-serif;
+        text-align: center;
     }
     .stButton>button {
         color: white;
         background-color: #4CAF50;
+    }
+    .main .block-container {
+        max-width: 800px; /* Adjust the max width of the central block */
+        margin: auto;
     }
     </style>
     """, unsafe_allow_html=True)
 
 st.title(':zap: Electricity Consumption and Production Prediction :zap:')
 
+# ------------------------------------------------------------------------------------------
+#                           Data Loading Functions
+# ------------------------------------------------------------------------------------------
 
-
-
-#########################################################
-# Load prediction unit ID mapping
 def load_pred_id_mapping():
+    """Load prediction unit ID mapping from a JSON file."""
     with open('streamlit/data/mapping_prediction_unit_id.json', 'r') as file:
         pred_id_mapping = json.load(file)
-    pred_id_mapping = pd.DataFrame.from_dict(pred_id_mapping, orient='index')
-    return pred_id_mapping
+    return pd.DataFrame.from_dict(pred_id_mapping, orient='index')
 
+def load_y_true():
+    """Load true data from a CSV file."""
+    y_true = pd.read_csv('streamlit/data/y_true.csv')
+    y_true['datetime'] = pd.to_datetime(y_true['datetime'])
+    return y_true
 
-# Load the y_true data
-y_true = pd.read_csv('streamlit/data/y_true.csv')
-y_true['datetime'] = pd.to_datetime(y_true['datetime'])
+# ------------------------------------------------------------------------------------------
+#                               Helper Functions
+# ------------------------------------------------------------------------------------------
 
+def get_county_number_from_name(county_name, county_name_mapping):
+    """Get county number from name."""
+    return county_name_mapping.get(county_name)
 
-pred_id_mapping = load_pred_id_mapping()
+def get_product_type_number(selected_product_name, product_type_mapping):
+    """Get product type number from name."""
+    return product_type_mapping.get(selected_product_name)
 
-pred_id_mapping = load_pred_id_mapping()
-print(pred_id_mapping)
-
-
-# Load county name mapping
-county_name_mapping = {
-    "0": "Harjumaa", "1": "Hiiumaa", "2": "Ida-Virumaa", "3": "Järvamaa",
-    "4": "Jõgevamaa", "5": "Lääne-Virumaa", "6": "Läänemaa", "7": "Pärnumaa",
-    "8": "Põlvamaa", "9": "Raplamaa", "10": "Saaremaa", "11": "Tartumaa",
-    "13": "Valgamaa", "14": "Viljandimaa", "15": "Võrumaa"
-}
-
-
-product_type_mapping = {'0': "Combined", '1': "Fixed", '2': "General service", '3': "Spot"}
-
-def get_county_number_from_name(county_name):
-    for num, name in county_name_mapping.items():
-        if name == county_name:
-            return num
-    return None
-
-
-def get_product_type_number(selected_product_name):
-    for num, name in product_type_mapping.items():
-        if name == selected_product_name:
-            return num
-    return None
-
-
-def find_pred_id(is_business, county_number, selected_product_type):
+def find_pred_id(is_business, county_number, selected_product_type, pred_id_mapping):
+    """Find prediction unit ID."""
     is_business_flag = 1 if is_business == 'Yes' else 0
     product_type_number = get_product_type_number(selected_product_type)
     for _, row in pred_id_mapping.iterrows():
-        if row['county'] == int(county_number) and \
-           row['is_business'] == is_business_flag and \
-           row['product_type'] == int(product_type_number):
+        if row['county'] == int(county_number) and row['is_business'] == is_business_flag and row['product_type'] == int(product_type_number):
             return row['prediction_unit_id']
     return None
 
-
-def get_available_product_types(county_number, is_business):
+def get_available_product_types(county_number, is_business, pred_id_mapping):
+    """Get available product types based on county number and business status."""
     is_business_flag = 1 if is_business == 'Yes' else 0
-    filtered_df = pred_id_mapping[(pred_id_mapping['county'] == int(county_number)) & 
-                                  (pred_id_mapping['is_business'] == is_business_flag)]
-    product_type_numbers = filtered_df['product_type'].unique().tolist()
-    return [product_type_mapping[str(num)] for num in product_type_numbers]
+    filtered_df = pred_id_mapping[(pred_id_mapping['county'] == int(county_number)) & (pred_id_mapping['is_business'] == is_business_flag)]
+    return [product_type_mapping[str(num)] for num in filtered_df['product_type'].unique().tolist()]
 
-def predict_electricity(start_date, end_date, prediction_unit_id):
-    pass
+# ------------------------------------------------------------------------------------------
+#                               Plotting Function
+# ------------------------------------------------------------------------------------------
 
-
-
-# Main page layout for inputs and prediction
-col1, col2, col3, col4 = st.columns([3, 3, 3, 3])
-
-# Inputs for prediction
-with col1:
-    date_range = st.date_input('Select Date Range', [datetime.date(2023, 5, 19), datetime.date(2023, 5, 20)])
-
-with col2:
-    is_business = st.selectbox('Is Business?', ['Yes', 'No'])
-
-with col3:
-    # Dropdown for county selection
-    county_name = st.selectbox('Select County', list(county_name_mapping.values()))
-    
-with col4:
-    county_number = get_county_number_from_name(county_name)
-    product_types = get_available_product_types(county_number, is_business)
-    selected_product_type = st.selectbox('Select Product Type', product_types)
-# Update product types based on selected county and business status
-
-
-
-
-
-
-import plotly.express as px
-import plotly.graph_objects as go
-
+# Initialize a plotly graph object figure
 def create_plotly_chart(y_true, y_pred, title, y_label):
-    # Initialize a plotly graph object figure
+    """Create a Plotly chart with true and predicted data."""
     fig = go.Figure()
 
     fig.add_trace(go.Scatter(
@@ -143,7 +94,6 @@ def create_plotly_chart(y_true, y_pred, title, y_label):
         name='Actual',
         line=dict(color='green')  
     ))
-
  
     if y_pred is not None:
         fig.add_trace(go.Scatter(
@@ -190,13 +140,48 @@ def create_plotly_chart(y_true, y_pred, title, y_label):
 
     return fig
 
+# ------------------------------------------------------------------------------------------
+#                                Main App Layout
+# ------------------------------------------------------------------------------------------
 
-#####################################################
-# Predict
-#####################################################
+# Load data
+pred_id_mapping = load_pred_id_mapping()
+y_true = load_y_true()
+
+# County and product type mappings
+county_name_mapping = {
+    "0": "Harjumaa", "1": "Hiiumaa", "2": "Ida-Virumaa", "3": "Järvamaa",
+    "4": "Jõgevamaa", "5": "Lääne-Virumaa", "6": "Läänemaa", "7": "Pärnumaa",
+    "8": "Põlvamaa", "9": "Raplamaa", "10": "Saaremaa", "11": "Tartumaa",
+    "13": "Valgamaa", "14": "Viljandimaa", "15": "Võrumaa"
+}
+product_type_mapping = {'0': "Combined", '1': "Fixed", '2': "General service", '3': "Spot"}
+
+# Input widgets for predictions
+col1, col2, col3, col4 = st.columns([3, 3, 3, 3])
+
+with col1:
+    date_range = st.date_input('Select Date Range', [datetime.date(2023, 5, 19), datetime.date(2023, 5, 20)])
+with col2:
+    is_business = st.selectbox('Is Business?', ['Yes', 'No'])
+
+with col3:
+    # Dropdown for county selection
+    county_name = st.selectbox('Select County', list(county_name_mapping.values()))
+    
+with col4:
+    county_number = get_county_number_from_name(county_name)
+    product_types = get_available_product_types(county_number, is_business)
+    selected_product_type = st.selectbox('Select Product Type', product_types) # Update product types based on selected county and business status
+
+
+# ------------------------------------------------------------------------------------------
+#                           Prediction and Visualization Logic
+# ------------------------------------------------------------------------------------------
+
 if st.button('Predict'):
     if len(date_range) == 2:
-        prediction_unit_id = find_pred_id(is_business, county_number, selected_product_type)
+        prediction_unit_id = find_pred_id(is_business, county_number, selected_product_type, pred_id_mapping)
         st.write(f'Prediction Unit ID: {prediction_unit_id}')
 
         # Filter y_true for the selected prediction_unit_id
@@ -231,9 +216,5 @@ if st.button('Predict'):
 
     else:
         st.error('Please select both a start and an end date.')
-
-
-
-
 
 
