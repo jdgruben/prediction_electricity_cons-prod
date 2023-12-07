cb5838714f3dd70decc4375d48f397564cc2d550
Jean de Gruben <jeandegruben@gmail.com>
2023-12-07 01:26:52 +0100

    V1

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,56 +1,67 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import pydeck as pdk
 import json
 import datetime
 import time
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-import plotly.express as px
 import plotly.graph_objects as go
 
-# ------------------------------------------------------------------------------------------
-#                           Set page configuration
-# ------------------------------------------------------------------------------------------
+#####################################################
+# STYLING AND PAGE CONFIGURATION
+#####################################################
 
 st.set_page_config(page_title='Electricity Prediction', layout='wide', page_icon=':zap:')
+
+# Define the style for the spinning effect
+st.markdown("""
+    <style>
+    @keyframes spin { 
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+    }
+    .spinning {
+        display: inline-block;
+        animation: spin 10s linear infinite;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+
+# CSS to center and position the title higher on the page
 st.markdown("""
     <style>
     body {
         font-family: 'Helvetica', sans-serif;
-        text-align: center;
     }
     .stButton>button {
         color: white;
         background-color: #4CAF50;
     }
-    .main .block-container {
-        max-width: 800px; /* Adjust the max width of the central block */
-        margin: auto;
+    h1 {
+        text-align: center;
+        margin-top: -50px;  /* Adjust the negative value to move title higher */
     }
     </style>
     """, unsafe_allow_html=True)
 
-st.title(':zap: Electricity Consumption and Production Prediction :zap:')
+# Centered title using markdown with HTML
+st.markdown('<h1><span class="spinning">⚡️</span> Electricity Consumption and Production Prediction <span class="spinning">⚡️</span></h1>', unsafe_allow_html=True)
 
-# ------------------------------------------------------------------------------------------
-#                           Data Loading Functions
-# ------------------------------------------------------------------------------------------
+#####################################################
+# DATA LOADING FUNCTIONS
+#####################################################
 
 def load_pred_id_mapping():
-    """Load prediction unit ID mapping from a JSON file."""
+    """Load the prediction unit ID mapping from a JSON file."""
     with open('streamlit/data/mapping_prediction_unit_id.json', 'r') as file:
         pred_id_mapping = json.load(file)
     return pd.DataFrame.from_dict(pred_id_mapping, orient='index')
 
-def load_y_true():
-    """Load true data from a CSV file."""
-    y_true = pd.read_csv('streamlit/data/y_true.csv')
-    y_true['datetime'] = pd.to_datetime(y_true['datetime'])
-    return y_true
+# Load the y_true data
+y_true = pd.read_csv('streamlit/data/y_true.csv')
+y_true['datetime'] = pd.to_datetime(y_true['datetime'])
+pred_id_mapping = load_pred_id_mapping()
 
-# County and product type mappings
+# Define county and product type mappings
 county_name_mapping = {
     "0": "Harjumaa", "1": "Hiiumaa", "2": "Ida-Virumaa", "3": "Järvamaa",
     "4": "Jõgevamaa", "5": "Lääne-Virumaa", "6": "Läänemaa", "7": "Pärnumaa",
@@ -59,42 +70,53 @@ county_name_mapping = {
 }
 product_type_mapping = {'0': "Combined", '1': "Fixed", '2': "General service", '3': "Spot"}
 
-# ------------------------------------------------------------------------------------------
-#                               Helper Functions
-# ------------------------------------------------------------------------------------------
+#####################################################
+# UTILITY FUNCTIONS
+#####################################################
 
 def get_county_number_from_name(county_name):
+    """Convert county name to its corresponding number."""
     for num, name in county_name_mapping.items():
         if name == county_name:
             return num
     return None
 
-def get_product_type_number(selected_product_name, product_type_mapping):
-    """Get product type number from name."""
-    return product_type_mapping.get(selected_product_name)
+def get_product_type_number(selected_product_name):
+    """Convert product type name to its corresponding number."""
+    for num, name in product_type_mapping.items():
+        if name == selected_product_name:
+            return num
+    return None
 
-def find_pred_id(is_business, county_number, selected_product_type, pred_id_mapping):
-    """Find prediction unit ID."""
+def find_pred_id(is_business, county_number, selected_product_type):
+    """Find prediction unit ID based on business flag, county number, and product type."""
     is_business_flag = 1 if is_business == 'Yes' else 0
     product_type_number = get_product_type_number(selected_product_type)
     for _, row in pred_id_mapping.iterrows():
-        if row['county'] == int(county_number) and row['is_business'] == is_business_flag and row['product_type'] == int(product_type_number):
+        if row['county'] == int(county_number) and \
+           row['is_business'] == is_business_flag and \
+           row['product_type'] == int(product_type_number):
             return row['prediction_unit_id']
     return None
 
-def get_available_product_types(county_number, is_business, pred_id_mapping):
-    """Get available product types based on county number and business status."""
+def get_available_product_types(county_number, is_business):
+    """Get available product types for a given county number and business status."""
     is_business_flag = 1 if is_business == 'Yes' else 0
-    filtered_df = pred_id_mapping[(pred_id_mapping['county'] == int(county_number)) & (pred_id_mapping['is_business'] == is_business_flag)]
-    return [product_type_mapping[str(num)] for num in filtered_df['product_type'].unique().tolist()]
+    filtered_df = pred_id_mapping[(pred_id_mapping['county'] == int(county_number)) & 
+                                  (pred_id_mapping['is_business'] == is_business_flag)]
+    product_type_numbers = filtered_df['product_type'].unique().tolist()
+    return [product_type_mapping[str(num)] for num in product_type_numbers]
 
-# ------------------------------------------------------------------------------------------
-#                               Plotting Function
-# ------------------------------------------------------------------------------------------
+def predict_electricity(start_date, end_date, prediction_unit_id):
+    """Function to predict electricity consumption and production."""
+    pass
 
-# Initialize a plotly graph object figure
+#####################################################
+# PLOTTING FUNCTIONS
+#####################################################
+
 def create_plotly_chart(y_true, y_pred, title, y_label):
-    """Create a Plotly chart with true and predicted data."""
+    """Create a Plotly chart for visualizing actual and predicted values."""
     fig = go.Figure()
 
     fig.add_trace(go.Scatter(
@@ -104,7 +126,7 @@ def create_plotly_chart(y_true, y_pred, title, y_label):
         name='Actual',
         line=dict(color='green')  
     ))
- 
+
     if y_pred is not None:
         fig.add_trace(go.Scatter(
             x=y_pred['datetime'], 
@@ -114,7 +136,6 @@ def create_plotly_chart(y_true, y_pred, title, y_label):
             line=dict(color='orange')  
         ))
 
-  
     fig.update_layout(
         title=title,
         xaxis_title="Time",
@@ -150,64 +171,51 @@ def create_plotly_chart(y_true, y_pred, title, y_label):
 
     return fig
 
-# ------------------------------------------------------------------------------------------
-#                                Main App Layout
-# ------------------------------------------------------------------------------------------
+#####################################################
+# MAIN PAGE LAYOUT AND INTERACTIVITY
+#####################################################
 
-# Load data
-pred_id_mapping = load_pred_id_mapping()
-y_true = load_y_true()
-
-# Input widgets for predictions
+# Layout for inputs
 col1, col2, col3, col4 = st.columns([3, 3, 3, 3])
 
+# Input widgets for prediction
 with col1:
     date_range = st.date_input('Select Date Range', [datetime.date(2023, 5, 19), datetime.date(2023, 5, 20)])
-    
+
 with col2:
     is_business = st.selectbox('Is Business?', ['Yes', 'No'])
 
 with col3:
-    # Dropdown for county selection
     county_name = st.selectbox('Select County', list(county_name_mapping.values()))
-    
+
 with col4:
     county_number = get_county_number_from_name(county_name)
     product_types = get_available_product_types(county_number, is_business)
     selected_product_type = st.selectbox('Select Product Type', product_types)
 
-
-# ------------------------------------------------------------------------------------------
-#                           Prediction and Visualization Logic
-# ------------------------------------------------------------------------------------------
-
+# Predict button and results display
 if st.button('Predict'):
     if len(date_range) == 2:
-        prediction_unit_id = find_pred_id(is_business, county_number, selected_product_type, pred_id_mapping)
+        prediction_unit_id = find_pred_id(is_business, county_number, selected_product_type)
         st.write(f'Prediction Unit ID: {prediction_unit_id}')
 
-        # Filter y_true for the selected prediction_unit_id
         filtered_data = y_true[y_true['prediction_unit_id'] == prediction_unit_id]
 
-        # Split data into consumption and production
         consumption_data = filtered_data[filtered_data['is_consumption'] == 1]
         production_data = filtered_data[filtered_data['is_consumption'] == 0]
 
-        # Pre-generate graphs
         fig1 = create_plotly_chart(consumption_data, None, 'Graph for Consumption', 'Consumption')
         fig2 = create_plotly_chart(production_data, None, 'Graph for Production', 'Production')
 
-        # Initialize progress bar
         progress_bar = st.empty()
         progress_emojis = ''
-        total_length = 42  
+        total_length = 42
 
         for i in range(total_length):
-            time.sleep(0.04)  
-            progress_emojis += ':zap:'  
+            time.sleep(0.04)
+            progress_emojis += ':zap:'
             progress_bar.markdown(f'<span style="font-size: 30px;">{progress_emojis}</span>', unsafe_allow_html=True)
 
-        # Display graphs after progress bar completes
         col1, col2 = st.columns([6, 6])
 
         with col1:
